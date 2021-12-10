fd178d96b85cc41f144fddaff5e5723d8f6c716b
Naser <rasyideight@gmail.com>
2021-12-10 16:51:01 +0700

    Halaman Landing Web

---
+++ Minggu7/landing.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>KRAKEN</title>
+    <link rel="stylesheet" href="style.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <!-- navbar start -->
+    <div class="navbar">
+      <div class="gb-brand">KRAKEN TEAM SOFTBALL ITERA</div>
+      <ul class="nav-link">
+        <li><a href="#" class="gb-link">HOME</a></li>
+        <li><a href="http://wa.me/0895320292922" class="gb-link">CONTACTS</a></li>
+        <li><a href="https://instagram.com/kraken.itera?utm_medium=copy_link" class="gb-link">ABOUT</a></li>
+      </ul>
+    </div>
+    <!-- navbar end -->
+    <!-- header start  -->
+    <section class="main">
+      <div class="container">
+        <div class="main-content">
+          <!-- <div>
+            <img src="./assets/img/kraken7.png" height="150px" />
+          </div> -->
+          
+          <div class="text">
+            <img src="./asset/kraken.png" height="220px" />
+            <h1><b>
+              KRAKEN TEAM SOFTBALL ITERA <br>
+              Membuka Pendaftaran Calon Anggota Baru !!!</b>
+              <div class="deskripsi1">
+                Jadwal latihan setiap hari Senin dan Kamis | 15.00 | Lp.Softball ITERA <br /><hr>
+                Bingung Apa itu Softball ??<br>
+                Atau Gabut Gada Kerjaan? <br>
+                Yuk, Dari pada ga produktif mending join KRAKEN biar ada aktivitas, hehehe
+              </div>
+            </h1>
+            <div class="btn-get-started">
+              <a href="tambah.php" class="btn">JOIN GEH</a>
+            </div>
+          </div>
+        </div>
+        <div class="banner">
+          <img src="./asset/softball2.png" height="320px" />
+        </div>
+      </div>
+    </section>
+    <!-- header end -->
+  
+  </body>
+</html>
